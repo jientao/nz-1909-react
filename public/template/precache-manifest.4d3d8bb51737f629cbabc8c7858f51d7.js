bbbb51d35c0e07bd55f16ebe5b67dcb8cfe301f3
jientao <2996619851@qq.com>
2020-04-15 12:40:19 +0800

    提交

---
+++ public/template/precache-manifest.4d3d8bb51737f629cbabc8c7858f51d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6122268a2d19ed7e109ca3aed8ed5493",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2586df1cb1007583bc9",
+    "url": "/static/css/main.71d6bda7.chunk.css"
+  },
+  {
+    "revision": "c3c9f708d1a8a59f08cc",
+    "url": "/static/js/2.d21b4ec9.chunk.js"
+  },
+  {
+    "revision": "90fb4deb912422b947a6490378160cce",
+    "url": "/static/js/2.d21b4ec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8d00aad6602d51c1b04",
+    "url": "/static/js/3.20cb6565.chunk.js"
+  },
+  {
+    "revision": "f2586df1cb1007583bc9",
+    "url": "/static/js/main.571a8ec8.chunk.js"
+  },
+  {
+    "revision": "7100fd5f8100712390b0",
+    "url": "/static/js/runtime-main.d51ff285.js"
+  }
+]);
